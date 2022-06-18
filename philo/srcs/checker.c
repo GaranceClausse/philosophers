27102464f3475ce1e5156d5c78f61e87929b4152
Garance CLAUSSE <gclausse@e1r4p22.42.fr>
2022-06-18 19:22:18 +0200

    Seems to be better

--- philo/srcs/checker.c
+++ philo/srcs/checker.c
@@ -19,12 +19,13 @@ void	check_death(t_philo *philo)
 	int				i;
 
 	i = 0;
-	
+	pthread_mutex_lock(&philo->param->smo_dead_mutex);
 	while (philo->param->smo_dead == 0 && i < philo->param->nb_philo)
 	{	
 		timestp = actual_time();
 		if (philo->param->philo[i]->meals != 0)
 		{
+			pthread_mutex_unlock(&philo->param->smo_dead_mutex);
 			lastmeal = (timestp - philo->param->philo[i]->ate_at);
 			if ((lastmeal >= philo->param->t_die && philo->param->smo_dead == 0
 				&& philo->param->philo[i]->state != EATING && philo->param->philo[i]->state != DONE)
@@ -36,9 +37,11 @@ void	check_death(t_philo *philo)
 			//	philo->param->philo[i]->state = STARVE;
 				printf("%lld %d %s (last meal = %lld, killed by %d)\n", (timestp - philo->param->start_at) /*(philo->param->philo[i]->ate_at + philo->param->t_die)*/, philo->param->philo[i]->id, "died", lastmeal, philo->id);
 			}
+			pthread_mutex_lock(&philo->param->smo_dead_mutex);
 		}
 		i++;
 	}
+	pthread_mutex_unlock(&philo->param->smo_dead_mutex);
 }
 
 int	check_num_philo(t_philo *philo)
